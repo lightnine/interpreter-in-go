9210faaf2b723fb1b8a9b4cc0e574b148f27c2f8
liliang21 <liliang21@baidu.com>
2023-02-03 21:19:48 +0800

    fix parse bug

--- parser/parser.go
+++ parser/parser.go
@@ -132,7 +132,7 @@ func (p *Parser) parseLetStatement() *ast.LetStatement {
 	p.nextToken()
 	stmt.Value = p.parseExpression(LOWEST)
 
-	for !p.curTokenIs(token.SEMICOLON) {
+	if p.peekTokenIs(token.SEMICOLON) {
 		p.nextToken()
 	}
 	return stmt
